828d3d935b3639053a96bdc8eb33c1562067e0ad
theodorus10823 <dannenberg.theo@gmail.com>
2016-05-13 17:32:19 +0200

    Update TheosQ.r

--- TheosQ/TheosQ.r
+++ TheosQ/TheosQ.r
@@ -1,11 +1,12 @@
+
 # clear variables and close windows
 rm(list = ls(all = TRUE))
 graphics.off()
 
 # install and load packages
-libraries <-  c("glmnet")
+libraries = c("glmnet")
 lapply(libraries, function(x) if (!(x %in% installed.packages())) {
-install.packages(x)
+  install.packages(x)
 })
 lapply(libraries, library, quietly = TRUE, character.only = TRUE)
 
@@ -13,24 +14,24 @@ lapply(libraries, library, quietly = TRUE, character.only = TRUE)
 data = read.table("carc.dat")
 
 # recode the response, y = 1 for y > 6000, otherwise y = 0
-y   <-  data[, 2]
-n <-  length(y)
-y   <-  ifelse(y <= 6000, 0, 1)
+y   = data[, 2]
+n   = length(y)
+y   = ifelse(y <= 6000, 0, 1)
 
-x1  <-  data[, 3]
-x2<-  data[, 4]
-x3 <-  data[, 5]
-x4   <-  data[, 6]
-x5  <-  data[, 7]
-x6  <- data[, 8]
-x7<-data[, 9]
-x8<-data[, 10]
-x9<-data[, 11]
-x10 <- data[, 12]
-x11 <- data[, 13]
-x12  <- data[, 14]
+x1  = data[, 3]
+x2  = data[, 4]
+x3  = data[, 5]
+x4  = data[, 6]
+x5  = data[, 7]
+x6  = data[, 8]
+x7  = data[, 9]
+x8  = data[, 10]
+x9  = data[, 11]
+x10 = data[, 12]
+x11 = data[, 13]
+x12 = data[, 14]
 
-x <- cbind(x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12)
+x = cbind(x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12)
 
 # lasso
 alpa = 1
